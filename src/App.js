64faae72008a5e4d88fe272d148905d94653a739
Estebahn Molina <estebahnmolina@estebahns-mbp.lan>
2022-07-05 16:06:37 -0500

    start of react app with pokemon api

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import PokemonList from "./PokemonList";
+import axios from "axios";
+
+function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+  const [nextPageUrl, setNextPageUrl] = useState( "https://pokeapi.co/api/v2/pokemon");
+  const [prevPageUrl, setPrevPageUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+  const [loading,setLoading]= useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    let cancel
+    axios.get(currentPageUrl, {cancelToken: new axios.CancelToken(c=> cancel=c)}).then((res) => {
+      setLoading(false);
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+       setPokemon(res.data.results.map((p) => p.name));
+    })
+    return ()  => cancel()
+  }, [currentPageUrl])
+
+  function goToNextPage(){
+     setCurrentPageUrl(nextPageUrl)
+  }
+  function goToPreviousPage() {
+    setCurrentPageUrl(prevPageUrl)
+
+
+  if (loading) return "Loading..."
+  
+ 
+
+  return (
+    <PokemonList pokemon={pokemon}/>
+  );
+}
+
+export default App;
